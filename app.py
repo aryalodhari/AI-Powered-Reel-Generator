cacf46fb554b3ad6aa8bfada24e70086f1d8238f
Arya Lodhari <aryalodhari224@gmail.com>
2026-01-31 17:31:06 +0530

    Added logic for file-upload

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from app.routes import main
 
+
 app = Flask(__name__)
+
+UPLOAD_FOLDER = 'user_uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 app.register_blueprint(main)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,10 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, current_app
 import uuid
+import os
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = {'png','jpg','jpeg'}
+
 
 main = Blueprint(
     "main",
@@ -13,11 +18,20 @@ def home():
 
 @main.route("/create", methods=["GET", "POST"])
 def create():
-    my_id = uuid
+    my_id = str(uuid.uuid1())
     if request.method == "POST":
+        rec_id = request.form.get("uuid")
+        text = request.form.get("text")
         print(request.files.keys())
         for key, value in request.files.items():
             print(key, value)
+            # upload file
+            file = request.files[key]
+            if file:
+                filename = secure_filename(file.filename)
+                os.mkdir(os.path.join(current_app.config['UPLOAD_FOLDER'], rec_id))
+                file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], rec_id, filename))
+
     return render_template("create.html", my_id=my_id)
 
 @main.route("/gallery")
